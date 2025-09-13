5a627f3484250e5d01d00b145202000b4c9e490f
caden li <cadenli2025@gmail.com>
2025-09-13 16:07:48 -0400

    agent v1

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,7 @@
 import { openai } from '@ai-sdk/openai';
 import { streamText, UIMessage, convertToModelMessages } from 'ai';
 
+
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import { Button } from "@/components/ui/button"
+'use client';
 
-export default function Home() {
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+
+export default function Page() {
+  const [generation, setGeneration] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div>
-      <Button>Click me</Button>
+      <Button
+        onClick={async () => {
+          setIsLoading(true);
+
+          await fetch('/api/completion', {
+            method: 'POST',
+            body: JSON.stringify({
+              prompt: 'There are several broken bricks in the sidewalk at 12 Greenwich Park, Roxbury, MA. The bricks are lifting up and pose a tripping hazard. I tripped and injured my knee and leg.',
+            }),
+          }).then(response => {
+            response.json().then(json => {
+              setGeneration(json);
+              setIsLoading(false);
+            });
+          });
+        }}
+        disabled={isLoading}
+      >
+        {isLoading ? 'Generating...' : 'Generate'}
+      </Button>
+
+      {isLoading ? (
+        'Loading...'
+      ) : (
+        <pre>{JSON.stringify(generation, null, 2)}</pre>
+      )}
     </div>
-  )
+  );
 }
