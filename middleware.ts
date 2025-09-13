816f30f40f229babb8d69a968f4b8040520b26b9
Soham Bafana <snbafana@gmail.com>
2025-09-13 14:44:40 -0400

    FIXED, new clean repo

--- middleware.ts
+++
@@ -1,7 +0,0 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({
-  publicRoutes: ["/", "/api/trpc/public.*"],
-});
-
-export const config = { matcher: ["/((?!_next|.*\\..*).*)"] };
