0db59b57cb6c2a8453c0a7763be809a0189648f3
Soham Bafana <snbafana@gmail.com>
2025-09-13 17:21:40 -0400

    Main commit

--- app/api/311/chat/route.ts
+++
@@ -1,6 +0,0 @@
-import { openai } from '@ai-sdk/openai';
-import { streamText, UIMessage, convertToModelMessages } from 'ai';
-
-
-// Allow streaming responses up to 30 seconds
-export const maxDuration = 30;
